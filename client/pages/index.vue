b15780b6ac560252f2810116ef96699064a09ca6
Chanhyuk Byeon <bdu00chch@gmail.com>
2022-11-05 21:12:18 +0900

    기본 메인페이지 (더미)

--- client/pages/index.vue
+++ client/pages/index.vue
@@ -1,3 +1,44 @@
+<script lang="ts" setup>
+const numberFormatter = Intl.NumberFormat('ko-KR');
+
+const todayScholarship = 456000;
+</script>
+
 <template>
-  <div></div>
+  <div class="py-12 px-8">
+    <div class="text-center my-8 mb-24">
+      <div class="text-2xl font-medium mb-3">오늘의 장학금</div>
+      <div class="text-5xl text-indigo-500 font-bold">{{ numberFormatter.format(todayScholarship) }} P</div>
+    </div>
+    <div class="mb-16">
+      <div class="text-2xl font-bold mb-3">장학글</div>
+      <div class="rounded bg-50 border border-gray-200 py-4 px-6">
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+      </div>
+    </div>
+    <div class="mb-16">
+      <div class="text-2xl font-bold mb-3">입학글</div>
+      <div class="rounded bg-50 border border-gray-200 py-4 px-6">
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+      </div>
+    </div>
+    <div class="mb-16">
+      <div class="text-2xl font-bold mb-3">휴학글</div>
+      <div class="rounded bg-50 border border-gray-200 py-4 px-6">
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+      </div>
+    </div>
+  </div>
 </template>
