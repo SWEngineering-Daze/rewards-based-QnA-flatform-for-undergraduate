6d1fe5d3e258bf1c3ffe125be460964722bf27a9
Chanhyuk Byeon <bdu00chch@gmail.com>
2022-11-09 00:55:36 +0900

    질문 상세보기 (1) - 제목 내용만 그냥 표시

--- client/pages/qna/[categoryType]/[categoryId]/[questionId]/index.vue
+++ client/pages/qna/[categoryType]/[categoryId]/[questionId]/index.vue
@@ -3,11 +3,19 @@ definePageMeta({
   middleware: ['auth'],
 });
 
+const { $axios } = useNuxtApp();
 const route = useRoute();
 
 const id = route.params.questionId;
+
+const {
+  data: { question },
+} = await $axios.get(`/questions/${id}`);
 </script>
 
 <template>
-  <div class="py-8 px-12">question's _id: {{ id }}</div>
+  <div class="py-8 px-12">
+    <h1 class="text-2xl font-bold mb-3">{{ question.title }}</h1>
+    <div>{{ question.content }}</div>
+  </div>
 </template>
