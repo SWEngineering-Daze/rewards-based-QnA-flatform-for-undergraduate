68886ffa4d62074be6f3e03b20a7e403b50d3562
Chanhyuk Byeon <bdu00chch@gmail.com>
2022-11-08 13:01:06 +0900

    test logging

--- client/composables/useToast.ts
+++ client/composables/useToast.ts
@@ -2,6 +2,7 @@ import * as pkg from 'vue-toastification';
 const { useToast: useToastOriginal } = pkg;
 
 export const useToast = () => {
+  console.log(useToastOriginal);
   const toast = useToastOriginal();
 
   return toast;
