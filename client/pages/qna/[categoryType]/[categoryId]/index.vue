015e07340b3f9fee0fb4ad603f1329bfe2dce559
Chanhyuk Byeon <bdu00chch@gmail.com>
2022-11-09 19:16:21 +0900

    페이지네이션 & refresh (TODO: usePagination)

--- client/pages/qna/[categoryType]/[categoryId]/index.vue
+++ client/pages/qna/[categoryType]/[categoryId]/index.vue
@@ -4,14 +4,40 @@ definePageMeta({
 });
 
 const { type, category } = await useCategory();
-const { $axios } = useNuxtApp();
 const route = useRoute();
 
-const page = Number.parseInt((route.query.page as string) ?? '1');
+const page = computed(() => {
+  return Number.parseInt((route.query.page as string) ?? '1');
+});
 
-const { data: questionPaginator } = await $axios.get(`/questions/${type}/${category.name}?page=${page}`);
-const questions = questionPaginator.questionList;
-const totalQuestions = questionPaginator.cntQuestions;
+const {
+  data: questionPaginator,
+  refresh,
+  pending,
+} = await useAsyncData(`question-${type}-${category.name}-${page.value}`, async ({ $axios }) => {
+  const { data } = await $axios.get(`/questions/${type}/${category.name}?page=${page.value}`);
+
+  return data;
+});
+
+watch(
+  () => route.query,
+  async () => {
+    await refresh();
+  }
+);
+
+const questions = computed(() => questionPaginator.value.questionList);
+const totalQuestions = computed(() => questionPaginator.value.cntQuestions);
+
+const perPage = 10; // from server
+const totalPages = Math.ceil(totalQuestions.value / perPage);
+const pagingNumbers: number[] = [];
+for (let i = page.value - 2; i <= page.value + 2; i++) {
+  if (i >= 1 && i <= totalPages) {
+    pagingNumbers.push(i);
+  }
+}
 </script>
 
 <template>
@@ -23,12 +49,23 @@ const totalQuestions = questionPaginator.cntQuestions;
       <template v-else>
         <NuxtLink class="block font-light text-blue-500 mb-2" :to="`/qna/department/${category.parent.name}`">{{ category.parent.name }}</NuxtLink>
       </template>
-      <h1 class="font-bold text-4xl">{{ category.name }}</h1>
+      <div class="flex items-center">
+        <h1 class="font-bold text-4xl">{{ category.name }}</h1>
+        <button class="ml-3 transition-transform hover:rotate-45" @click="refresh()">
+          <img class="w-6" src="@/assets/img/refresh.svg" alt="새로고침" />
+        </button>
+      </div>
     </div>
 
     <div class="text-xs text-gray-500 mb-4">총 {{ totalQuestions }}개</div>
 
-    <template v-if="questions.length > 0">
+    <template v-if="pending">
+      <div class="flex flex-col items-center text-center text-slate-500 tracking-wider">
+        <!-- spinner -->
+        <LoadingSpinner class="w-12" />
+      </div>
+    </template>
+    <template v-else-if="questions.length > 0">
       <div>
         <div class="flex items-center border-t border-b py-3 text-indigo-500 font-bold">
           <span class="subject-col">과목</span>
@@ -62,6 +99,19 @@ const totalQuestions = questionPaginator.cntQuestions;
       </div>
     </template>
 
+    <template v-if="pagingNumbers.length > 0">
+      <div class="flex justify-center my-8">
+        <NuxtLink
+          v-for="p in pagingNumbers"
+          :key="p"
+          :to="page === p ? undefined : `/qna/${type}/${category.name}?page=${p}`"
+          class="pagination-link"
+          :class="{ active: page === p }"
+          >{{ p }}</NuxtLink
+        >
+      </div>
+    </template>
+
     <template v-if="type === 'course'">
       <div class="flex justify-end my-8">
         <NuxtLink class="inline-block text-indigo-500 btn btn-primary" :to="`/qna/${type}/${category.name}/create`">작성하기</NuxtLink>
@@ -86,5 +136,12 @@ const totalQuestions = questionPaginator.cntQuestions;
 }
 .created-col {
   @apply ml-5 flex-shrink-0;
+}
+
+.pagination-link {
+  @apply inline-block py-2 px-5 text-white bg-indigo-500 bg-opacity-75 hover:bg-opacity-90 transition-all;
+}
+.pagination-link.active {
+  @apply bg-opacity-100;
 }
 </style>
