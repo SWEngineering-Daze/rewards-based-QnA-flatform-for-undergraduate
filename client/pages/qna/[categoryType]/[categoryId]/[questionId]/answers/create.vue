266e0b42de90ea7afe5c7e0ae19f22735d582215
Chanhyuk Byeon <bdu00chch@gmail.com>
2022-11-09 20:26:01 +0900

    답변 작성 페이지 구현

--- client/pages/qna/[categoryType]/[categoryId]/[questionId]/answers/create.vue
+++ client/pages/qna/[categoryType]/[categoryId]/[questionId]/answers/create.vue
@@ -1,9 +1,72 @@
 <script lang="ts" setup>
+import { AxiosError } from 'axios';
+import { useToast } from 'vue-toastification';
+
 definePageMeta({
   middleware: ['auth'],
 });
+
+const { $axios } = useNuxtApp();
+const toast = useToast();
+const route = useRoute();
+const router = useRouter();
+const { loading, startLoading, finishLoading } = useLoading();
+
+const content = ref('');
+
+const { data: question } = await useAsyncData(`answer-create-${route.params.questionId}`, async ({ $axios }) => {
+  const { data } = await $axios.get(`/questions/${route.params.questionId}`);
+
+  const question = data.question;
+
+  // nested property
+  return question;
+});
+
+async function submit() {
+  if (content.value === '') {
+    toast.error('내용을 작성해주세요!');
+    return;
+  }
+  try {
+    startLoading();
+
+    await $axios.post(`/questions/${question.value._id}/answers`, {
+      content: content.value,
+    });
+
+    toast.success(`성공적으로 답변을 작성했습니다!"`);
+    router.replace(`/qna/course/${question.value.course.name}/${question.value._id}`);
+  } catch (e) {
+    if (e instanceof AxiosError) {
+      toast.error('알 수 없는 네트워크 에러가 발생했습니다!');
+    } else {
+      toast.error('알 수 없는 에러가 발생했습니다!');
+
+      console.error(e);
+    }
+  } finally {
+    finishLoading();
+  }
+}
 </script>
 
 <template>
-  <div></div>
+  <div class="py-8 px-4 md:px-12">
+    <form class="max-w-5xl w-full mx-auto" @submit.prevent="submit">
+      <div class="mb-8 text-center">
+        <div class="text-sm text-indigo-500 mb-1">{{ question.course.parent.name }}</div>
+        <div class="text-xl">{{ question.course.name }}</div>
+      </div>
+      <h1 class="text-3xl text-center mb-6">{{ question.title }}</h1>
+      <div class="group">
+        <div class="text-lg font-medium mb-1">답변</div>
+        <textarea v-model="content" name="content" rows="30"></textarea>
+      </div>
+      <div class="flex">
+        <NuxtLink class="btn btn-link flex-1" :to="`/qna/course/${question.course.name}/${question._id}`">취소</NuxtLink>
+        <BaseButton class="flex-1" type="submit" :loading="loading">작성</BaseButton>
+      </div>
+    </form>
+  </div>
 </template>
