bd0266087d478ab0fcb85764b7a33dd7cdc5b352
Chanhyuk Byeon <bdu00chch@gmail.com>
2022-11-15 18:29:36 +0900

    fix: 스펠링

--- client/pages/qna/[categoryType]/[categoryId]/index.vue
+++ client/pages/qna/[categoryType]/[categoryId]/index.vue
@@ -107,7 +107,7 @@ for (let i = page.value - 2; i <= page.value + 2; i++) {
             <NuxtLink
               class="block text-indigo-500 text-opacity-75 transition-all hover:text-opacity-100"
               :to="`/qna/${type}/${encodeURIComponent(category.name)}/${question._id}`"
-              >{{ question.title }} [{{ question.answer.length ?? 'x' }}]</NuxtLink
+              >{{ question.title }} [{{ question.answers.length ?? 'x' }}]</NuxtLink
             >
           </div>
           <div class="created-col">
