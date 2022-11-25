6188f05d0e898445b91ae28af3923a68c9a0e57c
Chanhyuk Byeon <bdu00chch@gmail.com>
2022-11-25 10:59:02 +0900

    질문 수정 구현

---
+++ client/pages/questions/[questionId]/edit.vue
@@ -0,0 +1,137 @@
+<script lang="ts" setup>
+import { AxiosError } from 'axios';
+import { useToast } from 'vue-toastification';
+import { Question } from '@/composables/useApi';
+
+definePageMeta({
+  middleware: ['auth'],
+});
+
+const api = useApi();
+const toast = useToast();
+const route = useRoute();
+const router = useRouter();
+const { loading, startLoading, finishLoading } = useLoading();
+
+const questionId = computed(() => route.params.questionId as string);
+
+const title = ref('');
+const content = ref('');
+const filesToDelete = ref<string[]>([]);
+const files = ref<File[]>([]);
+
+const question = ref<Question>(null);
+try {
+  const qna = await api.questions.show(questionId.value);
+  question.value = qna.question;
+  title.value = question.value.title;
+  content.value = question.value.content;
+
+  console.log('question: ', question.value);
+} catch (e) {
+  if (e instanceof AxiosError) {
+    toast.error('알 수 없는 네트워크 에러가 발생했습니다.');
+
+    await navigateTo('/');
+  } else {
+    toast.error('알 수 없는 에러가 발생했습니다.');
+
+    console.error(e);
+  }
+}
+
+async function submit() {
+  if (title.value === '' || content.value === '') {
+    toast.error('제목과 내용을 모두 작성해주세요!');
+    return;
+  }
+
+  try {
+    startLoading();
+
+    await api.questions.update(
+      question.value._id,
+      {
+        title: title.value,
+        content: content.value,
+        filesToDelete: filesToDelete.value,
+      },
+      files.value
+    );
+
+    toast.success(`성공적으로 질문을 수정했습니다!`);
+    router.replace(`/qna/course/${question.value.course.name}/${question.value._id}`);
+  } catch (e) {
+    if (e instanceof AxiosError) {
+      toast.error('알 수 없는 네트워크 에러가 발생했습니다!');
+    } else {
+      toast.error('알 수 없는 에러가 발생했습니다!');
+
+      console.error(e);
+    }
+  } finally {
+    finishLoading();
+  }
+}
+
+function addFile(file: File) {
+  files.value.push(file);
+}
+
+function addDeletedFile(fileId: string) {
+  const idx = question.value.fileIds.findIndex(f => f === fileId);
+  question.value.fileIds.splice(idx, 1);
+  question.value.fileNames.splice(idx, 1);
+  filesToDelete.value.push(fileId);
+}
+
+function deleteFile(idx: number) {
+  files.value.splice(idx, 1);
+}
+</script>
+
+<template>
+  <div class="py-8 px-4 md:px-12">
+    <form class="max-w-5xl w-full mx-auto" @submit.prevent="submit">
+      <div class="mb-8 text-center">
+        <div class="text-indigo-500 mb-1">{{ question.course.parent.name }}</div>
+        <div class="text-2xl">{{ question.course.name }}</div>
+      </div>
+      <h1 class="text-3xl font-bold text-center mb-6">질문 수정</h1>
+      <div class="group">
+        <div class="text-lg font-medium mb-1">제목</div>
+        <input v-model="title" class="flex-1" type="text" name="email" placeholder="example" />
+      </div>
+      <div class="group">
+        <div class="text-lg font-medium mb-1">내용</div>
+        <MarkdownEditor v-model="content" name="content" />
+      </div>
+      <div class="group">
+        <div class="text-lg font-medium mb-3">첨부파일</div>
+        <div v-for="(fileId, idx) in question.fileIds" :key="fileId">
+          <span class="inline-flex items-center">
+            <img class="w-8" src="@/assets/img/attach.svg" />
+            <span class="ml-1 mr-5">{{ question.fileNames[idx] }}</span>
+            <button type="button" @click="addDeletedFile(fileId)">
+              <img class="w-8" src="@/assets/img/delete.svg" />
+            </button>
+          </span>
+        </div>
+        <div v-for="(file, idx) in files" :key="idx.toString() + file.name + file.type + file.size">
+          <span class="inline-flex items-center">
+            <img class="w-8" src="@/assets/img/attach.svg" />
+            <span class="ml-1 mr-5">{{ file.name }}</span>
+            <button type="button" @click="deleteFile(idx)">
+              <img class="w-8" src="@/assets/img/delete.svg" />
+            </button>
+          </span>
+        </div>
+        <BaseFileSelector @attach="addFile" />
+      </div>
+      <div class="flex">
+        <NuxtLink class="btn btn-link flex-1" :to="`/qna/course/${question.course.name}/${question._id}`">취소</NuxtLink>
+        <BaseButton class="flex-1" type="submit" :loading="loading">수정</BaseButton>
+      </div>
+    </form>
+  </div>
+</template>
